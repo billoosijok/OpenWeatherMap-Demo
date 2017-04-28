0c39afd01e67b2adb207a93ecbeb353c77210547
billoosijok <billoosijok@gmail.com>
2017-04-28 11:01:56 -0700

    Fully functional

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ $(function() {
 	// UI Outlets
 	var listDivContainer = $("#weather-result .container");
 	var template = document.getElementById("template");
+	var searchForm = $("#search-form");
+	var statusBar = $("#status-bar");
 
 	var api = new API_Connect({
 
@@ -11,29 +13,74 @@ $(function() {
 
 	});
 
-	api.request({
+	init();
 
-		'appid': api.apikey,
-		'q' : 'newyork',
-		'units' : 'imperial'
+	function init() {
 
-	}, function(result, status) {
-		
-		if(status == "success") {
+		var userLocation = {};
 
-			loadCurrentWeather(result);
-			console.log(result);
+		navigator.geolocation.getCurrentPosition(function(pos) {
+			
+			userLocation.lat = pos.coords.latitude;
+			userLocation.lon = pos.coords.longitude;
 
-		} else {
+			makeRquestWithParams(api, {
+				'appid': api.apikey,
+				'lat' : userLocation.lat,
+				'lon' : userLocation.lon,
+				'units' : 'imperial'
+			}, function(result) {
+				loadCurrentWeather(listDivContainer, result);
+			});
+		});
 
-		}
-				
-	});
+		searchForm.submit(function(e) {
+			e.preventDefault();
 
-	function loadCurrentWeather(data) {
-		listDivContainer.html(Mustache.to_html(template.innerHTML, data));
+			var query = searchForm.find("input[name='q']").val();
+
+			if(query.length) {
+					makeRquestWithParams(api, {
+					'appid'	: api.apikey,
+					'q'		: query,
+					'units' : 'imperial'
+				}, function(result) {
+					loadCurrentWeather(listDivContainer, result);
+				});
+			} else {
+				statusBar.html("<div class='error'>Please enter a location!</div>");
+			}
+		});
+
+		searchForm.find("input[name='q']").keydown(function(event) {
+			statusBar.html("");
+		});
 	}
 
+	function loadCurrentWeather(container, data) {
+		if(data) {
+			container.html(Mustache.to_html(template.innerHTML, data));
+		} else {
+			statusBar.html("<div class='error'>Couldn't find such loaction!</div>");
+		}
+	}
+
+	function makeRquestWithParams(api, params, callback) {
+		statusBar.html("<img src='resources/loading_boys.gif'>");
+		
+		api.request(params, function(result, status) {
+			
+			statusBar.html("");
+
+			if(status == "success") {
+				callback(result);
+				console.log(result);
+			} else {
+				callback(false)
+			}
+		});
+
+}
 });
 
 
